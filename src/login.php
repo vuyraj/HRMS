5e5db7ab2ad163a6fdb27983a16dc3bb93eeceb2
vuyraj <jarvuyhas@gmail.com>
2024-07-23 16:41:12 +0545

    Updated Login page html(initialized)

---
+++ src/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>login</title>
+        <!-- <meta name="description" content=""> -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- <link rel="stylesheet" href=""> -->
+    </head>
+    <body>
+        <h1>Welcome To Login Page</h1>
+
+        <br>
+        <form method=post>
+        <h4>Login:<h4>
+            <select id="usertype" name="usertype">
+                <option value="Owner">Owner</option>
+                <option value="Tenent">Tenant</option>
+            </select>
+
+            Username:
+            <input type="textbox" name="username" id="uname">
+            Password:
+            <input type= "password" name="password" id="passd">
+
+
+        </form>
+        <!-- <script src="" async defer></script> -->
+    </body>
+</html>
